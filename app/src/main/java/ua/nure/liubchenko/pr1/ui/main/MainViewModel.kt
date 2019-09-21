ad6aeff89cce882eb993c3d479bde0e1b31b0a2c
Yuriy Lyubchenko <undeadspez@gmail.com>
2019-09-21 04:18:47 +0300

    Fix 'color' propery

--- app/src/main/java/ua/nure/liubchenko/pr1/ui/main/MainViewModel.kt
+++ app/src/main/java/ua/nure/liubchenko/pr1/ui/main/MainViewModel.kt
@@ -4,6 +4,7 @@ import android.graphics.Color.*
 import androidx.lifecycle.*
 import ua.nure.liubchenko.pr1.utils.ColorUtils
 import ua.nure.liubchenko.pr1.utils.ColorUtilsImpl
+import androidx.lifecycle.LiveData
 
 class MainViewModel : ViewModel(), ColorUtils by ColorUtilsImpl {
 
@@ -11,31 +12,45 @@ class MainViewModel : ViewModel(), ColorUtils by ColorUtilsImpl {
         val defaultColor: Int = parseColor("#ffa500")
     }
 
-//    TODO: Doesn't work correctly
-//    val color: LiveData<Int> by lazy {
-//        MediatorLiveData<Int>().apply {
-//            value = 0xff shl 24
-//
-//            addSource(redComponent) { r ->
-//                this.apply { value = value!! or (r and 0xff shl 16) }
-//            }
-//
-//            addSource(greenComponent) { g ->
-//                this.apply { value = value!! or (g and 0xff shl 8) }
-//            }
-//
-//            addSource(blueComponent)  { b ->
-//                this.apply { value = value!! or (b and 0xff) }
-//            }
-//        }
-//    }
+    /**
+     * Previous implementation (wasn't working). TODO: Compare implementations.
+     * val color: LiveData<Int> by lazy {
+     *     MediatorLiveData<Int>().apply {
+     *         value = 0xff shl 24
+     *         addSource(redComponent)   { r -> value = value!! or (r and 0xff shl 16) }
+     *         addSource(greenComponent) { g -> value = value!! or (g and 0xff shl 8) }
+     *         addSource(blueComponent)  { b -> value = value!! or (b and 0xff) }
+     *     }
+     * }
+     */
+    val color: LiveData<Int> by lazy {
+        redComponent.switchMap { r ->
+            greenComponent.switchMap { g ->
+                blueComponent.switchMap { b ->
+                    arrayOf(
+                        r and 0xff shl 16,
+                        g and 0xff shl 8,
+                        b and 0xff
+                    ).reduce(Int::plus).let { MutableLiveData(it) }
+                }
+            }
+        }
+    }
 
-    //val hexColor: LiveData<String> = Transformations.map(color, this::rgbColorToHexString)
+    val hexColor: LiveData<String> by lazy {
+        Transformations.map(color, this::rgbColorToHexString)
+    }
 
-    val redComponent: MutableLiveData<Int>   = MutableLiveData(red(defaultColor))
+    val redComponent: MutableLiveData<Int> by lazy {
+        MutableLiveData(red(defaultColor))
+    }
 
-    val greenComponent: MutableLiveData<Int> = MutableLiveData(green(defaultColor))
+    val greenComponent: MutableLiveData<Int> by lazy {
+        MutableLiveData(green(defaultColor))
+    }
 
-    val blueComponent: MutableLiveData<Int>  = MutableLiveData(blue(defaultColor))
+    val blueComponent: MutableLiveData<Int> by lazy {
+        MutableLiveData(blue(defaultColor))
+    }
 
 }
--- app/src/main/java/ua/nure/liubchenko/pr1/utils/ColorUtils.kt
+++ app/src/main/java/ua/nure/liubchenko/pr1/utils/ColorUtils.kt
@@ -1,8 +1,5 @@
 package ua.nure.liubchenko.pr1.utils
 
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
-import android.graphics.drawable.Drawable
 import java.util.*
 
 interface ColorUtils {
@@ -14,8 +11,8 @@ object ColorUtilsImpl : ColorUtils {
     override fun rgbColorToHexString(color: Int): String = color.let {
         arrayOf(
             it shr 16 and 0xff,
-            it shr 8  and 0xff,
-            it        and 0xff
+            it shr 8 and 0xff,
+            it and 0xff
         ).map { component ->
             component
                 .toString(16)
@@ -23,7 +20,5 @@ object ColorUtilsImpl : ColorUtils {
                 .padStart(2, '0')
         }.fold("#", String::plus)
     }
-
-    fun Int.asDrawable(): Drawable = ColorDrawable(this)
 
 }
